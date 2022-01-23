bb8e6f2c9cc2d86c83cc782967ba74f90aeac58a
İshak Gönül <76657662+shakg@users.noreply.github.com>
2022-01-23 18:27:07 +0300

    UI enhancements
    - Added poppins font.
    - changed some visuals.

--- src/components/UavCard.vue
+++ src/components/UavCard.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="uav-card">
-      <p class="uav_id"> <b>UAV_ID_{{ uav_id }}</b> </p>
-      <div class="adhoc-section">
+      <h3 class="uav_id"> <b>UAV ID : {{ uav_id }}</b> </h3>
+      <div class="inner-section">
+          <div class="adhoc-section">
           <i class="ri-links-line child"></i>
           <p class="child">Ad-hoc IP : {{ adhoc_ip }}</p>
-      </div>
-      <div class="modal-section">
-          <i class="ri-plane-line child"></i>
-          <p class="child">Modal : {{ modal_name }}</p>
+            </div>
+            <div class="modal-section">
+                <i class="ri-plane-line child"></i>
+                <p class="child">Modal : {{ modal_name }}</p>
+            </div>
       </div>
   </div>
 </template>
-
-
 
 <script>
 import 'remixicon/fonts/remixicon.css';
@@ -29,29 +29,60 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .uav-card{
-    margin: 5px 20px 5px 20px;
-    padding : 10px;
+    margin: 15px 20px 15px 20px;
+    padding : 2px;
     border:.2px solid #ccc;
     border-radius: 6px;
-    align-content: center;
     align-items: center;
-    filter: drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.25));
+    vertical-align: middle;
+    align-self: center;
+    align-content: center;
+    display:flex;
+    flex-direction: column;
+    box-shadow: 10px 10px 5px grey;
+
+}
+.uav_id{
+    background-color: black;
+    color:white;
+    width: 98%;
+    padding:8px;
+    margin:0px;
+}
+.inner-section{
+    display:flex;
+    flex-direction: row;
+     align-items: center;
+    vertical-align: middle;
+    align-self: center;
+    align-content: center;
+}
+.adhoc-section {
+    padding:10px;
+    margin: 5px;
+    white-space: nowrap;
 }
 .modal-section {
     margin: 5px;
     white-space: nowrap;
-    border: .2px solid #ccc;
-    border-radius: 8px;
 }
-.adhoc-section {
-    margin: 5px;
-    white-space: nowrap;
-    border: .2px solid #ccc;
-    border-radius: 8px;
-}
+
 .child {
-    font-style:oblique;
     padding:4px;
     display: inline-block;
+}
+
+@media (max-width:800px) {    
+    .uav-card{
+        flex-direction: column;
+    }
+    .adhoc-section{
+        padding: 2px;
+        margin: 2px;
+    }
+    .modal-section{
+        padding: 2px;
+        margin: 2px;
+    }
 }
 </style>
